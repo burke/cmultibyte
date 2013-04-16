c7a84e593c81e0aa3081e85fcaa3f5704ec271d3
Burke Libbey <burke@libbey.me>
2013-04-16 16:31:30 -0500

    use ALLOC_N instead

--- ext/cmultibyte.c
+++ ext/cmultibyte.c
@@ -92,7 +92,7 @@ static VALUE tidy_bytes(VALUE string) {
   int i, j;
   uint8_t byte;
 
-  uint8_t *arr = ALLOCA_N(uint8_t, 4 * RSTRING_LEN(string));
+  uint8_t *arr = ALLOC_N(uint8_t, 4 * RSTRING_LEN(string));
   uint8_t *curr = arr;
   uint8_t *prev = curr;
   uint8_t *last_lead = curr;
@@ -155,6 +155,7 @@ static VALUE tidy_bytes(VALUE string) {
     }
   }
   VALUE str = rb_str_new((const char *)arr, curr-arr);
+  xfree(arr);
   return str;
 }
 
